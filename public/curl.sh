dc7f940389a350d7c82713dd89f879f8f83aff2e
Guille Vigil <guillermotti@Guilles-MacBook-Pro.local>
2020-11-26 19:10:19 +0100

    Minor fix on curl.sh

--- public/curl.sh
+++ public/curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -H "Content-Type: application/json" -XPUT "http://172.18.0.1:9200/_template/search_template" --data-binary "@/work/public/search_template.json"
+curl -H "Content-Type: application/json" -XPUT "http://elasticsearch:9200/_template/search_template" --data-binary "@/work/public/search_template.json"
 
-curl -H "Content-Type: application/x-ndjson" -XPOST "http://172.18.0.1:9200/index_search/_bulk" --data-binary "@/work/public/test_search.json"
+curl -H "Content-Type: application/x-ndjson" -XPOST "http://elasticsearch:9200/index_search/_bulk" --data-binary "@/work/public/test_search.json"
 
-curl -H "Content-Type: application/x-ndjson" -XPOST "http://172.18.0.1:9200/index_blog/_bulk" --data-binary "@/work/public/test_blog.json"
+curl -H "Content-Type: application/x-ndjson" -XPOST "http://elasticsearch:9200/index_blog/_bulk" --data-binary "@/work/public/test_blog.json"
